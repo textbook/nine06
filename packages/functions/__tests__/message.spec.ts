fb604e9c3d4839ca79cad9137edc2c4176e5fecf
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-12-03 20:27:38 +0000

    Add linting and fix violations

--- packages/functions/__tests__/message.spec.ts
+++ packages/functions/__tests__/message.spec.ts
@@ -2,14 +2,14 @@ import type { HandlerResponse } from "@netlify/functions";
 
 import { work as _work } from "@nine06/core";
 
-jest.mock("@nine06/core")
+jest.mock("@nine06/core");
 const work = jest.mocked(_work);
 
 import { handler } from "../message";
 
 describe("message handler", () => {
 	it("returns 200 OK", async () => {
-		work.mockResolvedValue("foo")
+		work.mockResolvedValue("foo");
 		await expect(handler())
 			.resolves.toEqual<HandlerResponse>({ body: "foo", statusCode: 200 });
 	});
