b0a484f300dfc35d456a6d3366c99036db52a16c
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2022-12-03 20:43:41 +0000

    Switch to scheduled function

--- packages/functions/__tests__/message.spec.ts
+++ packages/functions/__tests__/message.spec.ts
@@ -1,16 +1,10 @@
 import type { HandlerResponse } from "@netlify/functions";
 
-import { work as _work } from "@nine06/core";
-
-jest.mock("@nine06/core");
-const work = jest.mocked(_work);
-
 import { handler } from "../message";
 
 describe("message handler", () => {
 	it("returns 200 OK", async () => {
-		work.mockResolvedValue("foo");
 		await expect(handler())
-			.resolves.toEqual<HandlerResponse>({ body: "foo", statusCode: 200 });
+			.resolves.toEqual<HandlerResponse>({ statusCode: 200 });
 	});
 });
--- packages/functions/message.ts
+++ packages/functions/message.ts
@@ -1,10 +1,7 @@
 import type { HandlerResponse } from "@netlify/functions";
 
-import { work } from "@nine06/core";
-
 const handler = async (): Promise<HandlerResponse> => {
-	const body = await work();
-	return { body, statusCode: 200 };
+	return { statusCode: 200 };
 };
 
 export { handler };
